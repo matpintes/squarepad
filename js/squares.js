f35803540b32a3bceaf7c0d64fe513d1c5a51157
Matus Pintes <pintes.matus@gmail.com>
2017-07-07 18:12:54 +0200

    Add canvas reset

--- js/squares.js
+++ js/squares.js
@@ -1,12 +1,36 @@
-grid = 256;
+count = 12;
 
-$(document).ready(function(){
+$(document).ready(function() {
+	// Initialize
+	repaint();
+
+	$('#reset-btn').on('click', function(){
+		count = prompt("Enter number of squares per side");
+		if(count == null) count = 12;
+		repaint();
+	});
+});
+
+
+
+
+
+
+
+
+function repaint() {
+	// Remove all child elements
+	$('.container').empty();
+	// Calculate width of a single square
+	var size = $('.container').width() / count;
 	// Populate container
-	for(var i = 0; i < grid; i++)
-		$('.container').append('<div class="square"></div>');
-
-	// Event handler
+	for(var i = 0; i < count; i++) {
+		for(var j = 0; j < count; j++) {
+			$('.container').append('<div class="square" style="width: '+size+'px; height: '+size+'px"></div>');
+		}
+	}
+	// Attach event handler
 	$('.square').hover(function(){
 		$(this).addClass('painted');
-	}, null)
-});
+	}, null);
+}
